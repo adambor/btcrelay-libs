c509fdbe4d9414f760192d47fa867045bfc61884
adambor <adamborteam@gmail.com>
2024-04-11 10:21:50 +0200

    Bump base lib version

--- btcrelay-bitcoind/src/synchronizer/BitcoindRelaySynchronizer.ts
+++ btcrelay-bitcoind/src/synchronizer/BitcoindRelaySynchronizer.ts
@@ -59,23 +59,29 @@ export class BtcRelaySynchronizer<V extends BtcStoredHeader<any>, T> implements 
         };
         const computedHeaderMap: {[blockheight: number]: V} = {};
 
+        let forkFee: string;
+        let mainFee: string;
+
         const saveHeaders = async (headerCache: BitcoindBlock[], final: boolean) => {
             console.log("[BtcRelaySynchronizer]: Header cache: ", headerCache.map(e => e.hash));
             if(cacheData.forkId===-1) {
+                if(mainFee==null) mainFee = await this.btcRelay.getMainFeeRate();
                 if(
                     final &&
                     this.btcRelay.maxShortForkHeadersPerTx!=null &&
                     this.btcRelay.maxShortForkHeadersPerTx>=headerCache.length &&
                     this.btcRelay.saveShortForkHeaders!=null
                 ) {
-                    cacheData = await this.btcRelay.saveShortForkHeaders(headerCache, cacheData.lastStoredHeader, tipData.chainWork);
+                    cacheData = await this.btcRelay.saveShortForkHeaders(headerCache, cacheData.lastStoredHeader, tipData.chainWork, mainFee);
                 } else {
-                    cacheData = await this.btcRelay.saveNewForkHeaders(headerCache, cacheData.lastStoredHeader, tipData.chainWork);
+                    cacheData = await this.btcRelay.saveNewForkHeaders(headerCache, cacheData.lastStoredHeader, tipData.chainWork, mainFee);
                 }
             } else if(cacheData.forkId===0) {
-                cacheData = await this.btcRelay.saveMainHeaders(headerCache, cacheData.lastStoredHeader);
+                if(mainFee==null) mainFee = await this.btcRelay.getMainFeeRate();
+                cacheData = await this.btcRelay.saveMainHeaders(headerCache, cacheData.lastStoredHeader, mainFee);
             } else {
-                cacheData = await this.btcRelay.saveForkHeaders(headerCache, cacheData.lastStoredHeader, cacheData.forkId, tipData.chainWork)
+                if(forkFee==null) forkFee = await this.btcRelay.getForkFeeRate(cacheData.forkId);
+                cacheData = await this.btcRelay.saveForkHeaders(headerCache, cacheData.lastStoredHeader, cacheData.forkId, tipData.chainWork);
             }
             if(cacheData.forkId!==-1 && cacheData.forkId!==0) startForkId = cacheData.forkId;
             txsList.push(cacheData.tx);
